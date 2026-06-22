2e168f0e1aa8a743e8d56beaaa06ae5a8ae4b136
Tanishq <tanishqb034@gmail.com>
2026-06-22 16:47:26 +0530

    THIS IS MY TUPLE BASICS

---
+++ Tuple/basic1.py
@@ -0,0 +1,51 @@
+t=(10,20,30,40,50)
+print(t)
+print(type(t))
+#print last element tuple
+print(t[-1])
+#print last element tuple
+print(t[0])
+
+
+# print 3 element
+print(t[2])
+#print len of tuple
+
+print(len(t))
+
+#print looping in tuple
+for i in t:
+    print(i)
+    
+    #check tuple has element
+    
+print(50 in t)  
+
+#check 100 in tuple or not
+
+print(100 in t)  
+
+#max in tuple
+print(max(t))
+
+#min in tuple 
+print(min(t))
+
+#sum of tuple
+print(sum(t))
+#function
+sr=0
+for i in t:
+    sr+=i
+    
+print(sr)
+#slicing in tuple 
+#first 3 element
+print(t[:3])
+
+#skip 1 -1 element
+print(t[:len(t):2])
+
+#last 3 element
+
+print(t[-3:])
