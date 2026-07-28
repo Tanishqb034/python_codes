25bf583f8b37deb38daea62d9054661965d843f5
Tanishq <tanishqb034@gmail.com>
2026-07-28 19:50:38 +0530

    THIS IS A NEW

--- keyword_argument_fun1.py
+++ keyword_argument_fun1.py
@@ -3,3 +3,9 @@ def Student(name,age):
     print("Age: ",age)
     
 Student(age=21,name="Tanishq")    
+
+def Str(name,age):
+    print("Name: ",name)
+    print("age: ",age)
+    
+Str(age=33,name="Tanishq")   
