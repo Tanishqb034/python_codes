bbadbafc41874cf2a9a7d8f2660eb77f877c50a7
Tanishq <tanishqb034@gmail.com>
2026-08-25 19:57:51 +0530

    RESUME TOMORROW

--- regexp1.py
+++ regexp1.py
@@ -1,5 +1,9 @@
 import re
 pattern=r"\d{10}"
+name_pattern = r"^[A-Za-z ]+$"
+age_pattern = r"^\d+$"
+email_pattern = r"^[\w.-]+@[\w.-]+\.\w+$"
+
 text="9928996660"
 result=re.search(pattern,text)
 print(result)     
