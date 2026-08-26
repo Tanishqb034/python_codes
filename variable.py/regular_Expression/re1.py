11eaf63b5834f0f542125a8e7ee2e5a091896063
Tanishq <tanishqb034@gmail.com>
2026-08-26 18:56:01 +0530

    THIS IS MATCH FUNCTION

--- variable.py/regular_Expression/re1.py
+++ variable.py/regular_Expression/re1.py
@@ -1,7 +1,15 @@
 import re
 
 text="TANISHQ BHARDWAJ Python"
-pattern="Python"
+pattern="TANISHQ"
 result=re.search(pattern,text)
 print(result)
 print(result.start())
+print(result.end())
+if result:
+    print("MATCH")
+else:
+    print("Not MATCH")    
+    
+result_2=re.match(pattern,text)  
+print(result_2.group())
