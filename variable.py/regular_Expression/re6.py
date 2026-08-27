3df705a308ed077425c187d7b3be5ebc4366f46c
Tanishq <tanishqb034@gmail.com>
2026-08-27 19:53:47 +0530

    THIS IS MY NAME PATTERN

--- variable.py/regular_Expression/re6.py
+++ variable.py/regular_Expression/re6.py
@@ -1,6 +1,6 @@
 import re
 pattern=r"[a-z]"
-text="THIS 7 is my hobby 12@#$"
+text="THIS 7 is mmy hobby 12@#$"
 result=re.findall(pattern,text)
 print(result)
 pattern2=r"[A-Z]"
@@ -12,3 +12,7 @@ print(result3)
 allinone=r"[a-zA-Z0-9]"
 result4=re.findall(allinone,text)
 print(result4)
+name_pat=r"^[A-Za-z ]+$"
+name="TANISHQ BHARDWAJ"
+result_name=re.findall(name_pat,name)
+print(result_name)
