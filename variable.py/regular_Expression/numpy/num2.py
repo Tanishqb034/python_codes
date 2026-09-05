1ae418d9e17185e7a75eeace958e1d884be313a7
Tanishq <tanishqb034@gmail.com>
2026-09-05 19:37:14 +0530

    THIS IS MY CODE

--- variable.py/regular_Expression/numpy/num2.py
+++ variable.py/regular_Expression/numpy/num2.py
@@ -5,3 +5,22 @@ b=np.array([
     [1,2,3],[4,5,6]
 ])
 print(b)
+c=np.zeros((2,3))
+print(c)  #zeroes matrix
+d=np.ones((3,3))
+print(d)   #one matrix
+
+e=np.eye(3)
+print(e) #identity matrix
+e=np.diag([1,1])
+print(e)
+f=np.full((2,2),10)
+print(f)
+g=np.empty((2,2))
+print(g)
+h=np.arange(1,10)
+print(h)
+i=np.arange(0,20,2)
+print(i)
+j=np.linspace(0,10,5)
+print(j)
