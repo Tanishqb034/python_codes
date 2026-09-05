756d830816b0238debcabe5ddc786109a6893524
Tanishq <tanishqb034@gmail.com>
2026-09-05 18:40:49 +0530

    THIS IS MY TIME

---
+++ variable.py/regular_Expression/numpy/num1.py
@@ -0,0 +1,18 @@
+import numpy as np
+arr_1d = np.array([1, 2, 3, 4, 5])
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+
+zeros = np.zeros((3, 4))
+
+
+ones = np.ones((2, 3))
+
+constants = np.full((2, 2), 7)
+
+
+sequence = np.arange(0, 10, 2)
+
+linspace = np.linspace(0, 1, 5)
+
+identity = np.eye(3)
+print(identity)
