8fa1e61b702ef8171310903c28ad3c2100b90156
Tanishq <tanishqb034@gmail.com>
2026-09-12 18:53:17 +0530

    THIS IS MY Pattern

--- variable.py/regular_Expression/re3.py
+++ variable.py/regular_Expression/re3.py
@@ -1,5 +1,8 @@
 import re
 pattern=r"\D+"
+pattern2=r"\d+"
 text="I HAVE 4 Apple 10 ORANGES AND 5 bananan "
 result=re.findall(pattern,text)
+result1=re.findall(pattern2,text)
+print(result1)
 print(result)
